6012df1dafc260f154f4bb6c31e418666b16af57
Song <31345708+1noob@users.noreply.github.com>
2024-01-22 00:03:10 +0800

    Update handlers.ts

--- src/handlers.ts
+++ src/handlers.ts
@@ -6,7 +6,7 @@ import { UserDetails } from './interfaces/interface';
 
 export class Handlers {
     public getRoot(_req: Request, res: Response) {
-        res.send(`<h1>GitHub Readme Activity Graph 📈</h1>`);
+        res.send(`<></>`);
     }
 
     public async getGraph(req: Request, res: Response) {
